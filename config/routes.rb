1ca39593209abdf6ece92b2de109beaaa1714059
Shamel Lakin <christlife4u@gmail.com>
2020-08-28 12:32:41 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'categories/show'
+
+  resources :comments
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+  resources :categories
+  
 end
